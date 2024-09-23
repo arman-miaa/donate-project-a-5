da46d58a91cb904ba9d6aeabf01bf117f7468bde
Arman <mdarman365506@gmail.com>
2024-09-23 08:48:54 +0600

    first commit and set all link

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+  
+    tailwind.config = {
+        theme: {
+          
+            extend: {
+              fontFamily: {
+              lexend: ['Lexend', 'sans-serif'],
+          },
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+  
