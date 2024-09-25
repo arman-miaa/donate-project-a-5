7ca1813eef53dbfb9347aabd2ae30a38d7ed74a0
Arman <mdarman365506@gmail.com>
2024-09-25 12:41:42 +0600

    all section added

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ btnDonate.addEventListener("click", function (event) {
 
 btnHistory.addEventListener('click', function (event) {
   event.preventDefault();
-  console.log(historyContainer);
   historyContainer.classList.add('flex');
   historyContainer.classList.remove('hidden');
   donateContainer.classList.add('hidden');
@@ -66,23 +65,23 @@ const handleDonation = (inputVal, val, title) => {
   }
 };
 //  first
-const donateBtnOne = (event) => {
+const donateBtnOne = function() {
   let inputAmount1 = CommonFunction("donation-input-1");
   let title1 = CommonFunction("title-Text-1");
-  const inputField1 = parseInt(CommonFunction("input-box-1").value);
+  const inputField1 = Number(CommonFunction("input-box-1").value);
   handleDonation(inputField1, inputAmount1, title1);
 };
 //  second
-const donateBtnTwo = (event) => {
+const donateBtnTwo = function()  {
   let inputAmount2 = CommonFunction("donation-input-2");
   let title2 = CommonFunction("title-Text-2");
-  const inputField2 = parseInt(CommonFunction("input-box-2").value);
+  const inputField2 = Number(CommonFunction("input-box-2").value);
   handleDonation(inputField2, inputAmount2, title2);
 };
 //  third
-const donateBtnThree = (event) => {
+const donateBtnThree = function() {
   let inputAmount3 = CommonFunction("donation-input-3");
   let title3 = CommonFunction("title-Text-3");
-  const inputField3 = parseInt(CommonFunction("input-box-3").value);
+  const inputField3 = Number(CommonFunction("input-box-3").value);
   handleDonation(inputField3, inputAmount3, title3);
 };
